41086f3eca998d322b6e096ed5de2732a573c850
Ryan Stanley <stanleyrya@gmail.com>
2015-12-01 02:31:55 -0500

    added movie cards for main page with sample data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var movies = [
+  'John Cena',
+  'Ryan Stanley',
+  'John Cena',
+  'Ryan Stanley',
+  'John Cena',
+  'Ryan Stanley',
+  'John Cena',
+  'Ryan Stanley'
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { movies: movies });
 });
 
 module.exports = router;
