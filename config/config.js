34eae3b4f0cc284e41a2b6ac53e511806bb868e2
Ryan Stanley <stanleyrya@gmail.com>
2015-12-03 12:48:11 -0500

    switched database to postgres, added production database (heroku)

--- config/config.js
+++ config/config.js
@@ -1,23 +1,22 @@
-{
+module.exports = {
   "development": {
     "username": "root",
     "password": null,
     "database": "database_development",
     "host": "127.0.0.1",
-    "dialect": "sqlite"
+    "dialect": "postgres"
   },
   "test": {
     "username": "root",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "sqlite"
+    "dialect": "postgres"
   },
   "production": {
     "username": "root",
     "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "sqlite"
+    "host": process.env.DATABASE_URL,
+    "dialect": "postgres"
   }
 }
